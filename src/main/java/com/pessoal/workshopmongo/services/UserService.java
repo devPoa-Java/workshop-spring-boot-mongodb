69f1054e68d813a114bc04eaf684e98b1995c21a
Sandro M. dos Santos <sandromarista@gmail.com>
2025-07-31 17:48:03 -0300

    Atualização de usuário com PUT

--- src/main/java/com/pessoal/workshopmongo/services/UserService.java
+++ src/main/java/com/pessoal/workshopmongo/services/UserService.java
@@ -15,26 +15,37 @@ import com.pessoal.workshopmongo.services.exception.ObjectNotFoundException;
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
-	 return	userRepository.findAll();		
+
+	public List<User> findAll() {
+		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
-	public void  delete(String id) {
+
+	public void delete(String id) {
 		userRepository.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User user = findById(obj.getId());
+		updateData(user, obj);
+		return userRepository.save(user);
+	}
+
+	private void updateData(User user, User obj) {
+		user.setName(obj.getName());
+		user.setEmail(obj.getEmail());
 	}
 
 }
